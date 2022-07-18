ecb6c09eeab8c3940cf5ef238aac56839375f5d6
Mateusz Smykowski <smykowskimateusz@gmail.com>
2022-07-18 18:18:30 +0200

    Created profile and address model

---
+++ Shop/Models/Profile.cs
@@ -0,0 +1,11 @@
+﻿namespace Shop.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Avatar { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
