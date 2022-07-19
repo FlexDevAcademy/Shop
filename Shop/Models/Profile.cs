97f1fc689ed5b30a11735090cc05c6d1ab3ed44a
Mateusz Smykowski <smykowskimateusz@gmail.com>
2022-07-19 17:19:32 +0200

    Added property Email in Profile model

--- Shop/Models/Profile.cs
+++ Shop/Models/Profile.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        public string Email { get; set; }
         public string Avatar { get; set; }
         public decimal Balance { get; set; }
         public Address Address { get; set; }
