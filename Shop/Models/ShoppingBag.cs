3ff1f2bc4922131452fe677c483e6dde8a9c9a1a
Mateusz Smykowski <smykowskimateusz@gmail.com>
2022-07-19 19:19:04 +0200

    Added ShoppingBag and modiefied Item

---
+++ Shop/Models/ShoppingBag.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Shop.Models
+{
+    public class ShoppingBag
+    {
+        public int Id { get; set; }
+        public int ProfileId { get; set; }
+        public List<Item> Items { get; set; }
+    }
+}
